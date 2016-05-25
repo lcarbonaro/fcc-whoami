0f8227529c659c112dea312155a225ed5ad979dc
Les Carbonaro <lcarbonaro@gmail.com>
2016-05-25 19:26:33 +0000

    updated

--- server.js
+++ server.js
@@ -12,13 +12,16 @@ server.get('/', function(req,res){
 });
 
 server.get('/api/whoami', function(req, res) {
-    
+
+    // console.log(req.ip);  gives ::ffff:10.240.0.200
     var reqIP = req.ip;
     var ipArr = reqIP.split(':');
     var ip = ipArr[ ipArr.length - 1 ];
     
+    // console.log(req.headers['accept-language']);  gives  en-US,en;q=0.8
     var lang = req.headers['accept-language'].split(',')[0];
     
+    // console.log(req.headers['user-agent']);  gives  Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) etc.
     var os = req.headers['user-agent'].split('(')[1].split(')')[0];
     
     var o = {
